e90665f7a31af6220a148567c1218af4b44f4ee6
Pavel Kasila <pavel.kasila@gmail.com>
2022-06-06 18:58:14 +0300

    RabbitMQ: set routing_key; add dotenv configuration support, cleanup

--- rmq_communication.py
+++ rmq_communication.py
@@ -1,23 +1,36 @@
+import os
 import pika
-import sys
-from rmq_parameters import *
 import json
 import requests
-import io
 from get_text import get_text, preprocessing
 import utils
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = os.getenv("HOST")
+SENDER_QUEUE_NAME = os.getenv("SENDER_QUEUE_NAME")
+CONSUMER_QUEUE_NAME = os.getenv("CONSUMER_QUEUE_NAME")
+
+print(HOST, SENDER_QUEUE_NAME, CONSUMER_QUEUE_NAME)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(HOST))
 
 rmq_channel = connection.channel()
 session = requests.Session()
 
+rmq_channel.exchange_declare(exchange=SENDER_QUEUE_NAME, exchange_type='topic', durable=True)
+
+
 def send(message: str) -> None:
-    rmq_channel.basic_publish("", SENDER_QUEUE_NAME, message)
+    print(message)
+    rmq_channel.basic_publish(exchange=SENDER_QUEUE_NAME, routing_key=SENDER_QUEUE_NAME, body=message)
+
 
 def on_message(channel, method_frame, header_frame, body) -> None:
+    rmq_channel.basic_ack(method_frame.delivery_tag)
     data = json.loads(body)
-    if (not utils.download_image(session, data["sourceUploadURL"], "temp.png")):
+    if not utils.download_image(session, data["sourceUploadURL"], "temp.png"):
         return None
     rect = data["rect"]
     data.pop("sourceUploadURL")
@@ -25,9 +38,15 @@ def on_message(channel, method_frame, header_frame, body) -> None:
     data["data"]["text"] = get_text(preprocessing("temp.png", [int(rect["x"]), int(rect["y"]),
                                                                int(rect["width"]), int(rect["height"])]))
     send(json.dumps(data, separators=(',', ':'), ensure_ascii=False))
-    rmq_channel.basic_ack(method_frame.delivery_tag)
 
-rmq_channel.basic_consume(CONSUMER_QUEUE_NAME, on_message)
+
+result = rmq_channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+rmq_channel.queue_bind(exchange=CONSUMER_QUEUE_NAME, queue=queue_name, routing_key=CONSUMER_QUEUE_NAME)
+
+rmq_channel.basic_consume(
+    queue=queue_name, on_message_callback=on_message, auto_ack=False)
 
 try:
     rmq_channel.start_consuming()
