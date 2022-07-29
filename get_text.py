cc92917316a5230810ddbba6bff8488bcc7ee14f
Pavel Kasila <pavel.kasila@gmail.com>
2022-07-29 16:21:49 +0300

    Crop using Pillow

--- get_text.py
+++ get_text.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import pytesseract
-import cv2
 
 
 # all images must be in .png format
@@ -10,11 +9,7 @@ def preprocessing(image: str, c: list = []) -> str:
        c - cropping, if len(c) = 0 cropping is not required\n
        otherwise len(c) = 4, (x, y, w, h)\n
     """
-    img = cv2.imread(image, 0)
-    if len(c) != 0:
-        img = img[c[1]:c[1] + c[3], c[0]:c[0] + c[2]]
-    _, res = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    cv2.imwrite(image, res)
+    Image.open("temp.png").crop((c[0], c[1], c[0] + c[2], c[1] + c[3])).save(image)
     return image
 
 
